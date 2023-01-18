7624b813900983acce4de9df60a7f0885c76b108
DMzartex <190478@site.asty-moulin.be>
2023-01-18 22:19:12 +0100

    ajout calc binaire objet

---
+++ Calc_Binaire_Objet/Calc_Binaire_Objet/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calc_Binaire_Objet
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            nbr1.PreviewTextInput += new TextCompositionEventHandler(PreviewNbr);
+            nbr2.PreviewTextInput += new TextCompositionEventHandler(PreviewNbr);
+            addition.Checked += new RoutedEventHandler(CheckAdd);
+            soustraction.Checked += new RoutedEventHandler(CheckSoustra);
+            calculer.Click += new RoutedEventHandler(ClickCalc);
+        }
+
+        int typeCalc;
+
+        private void PreviewNbr(object sender, TextCompositionEventArgs e)
+        {
+            if (e.Text != "0" && e.Text != "1")
+            {
+                e.Handled = true;
+            }
+            else
+            {
+                e.Handled = false;
+            }
+        }
+
+        private void CheckAdd(object sender, RoutedEventArgs e)
+        {
+            typeCalc = 0;
+        }
+
+        private void CheckSoustra(object sender, RoutedEventArgs e)
+        {
+            typeCalc = 1;
+        }
+
+        private void ClickCalc(object sender, RoutedEventArgs e)
+        {
+            NombreBinaire nombre1 = new NombreBinaire(nbr1.Text);
+            NombreBinaire nombre2 = new NombreBinaire(nbr2.Text);
+            NombreBinaire tRes;
+            bool ok;
+
+            if(typeCalc == 0)
+            {
+                nombre1.Additionne(nombre2,out tRes,out ok);
+                if (ok)
+                {
+                    MessageBox.Show(tRes.AffichageResultat());
+                }
+            }else if(typeCalc == 1)
+            {
+                ok = nombre1.Soustrait(nombre2, out tRes);
+                if (ok)
+                {
+                    MessageBox.Show(tRes.AffichageResultat());
+                }
+            }
+        }
+    }
+}
---
+++ Calc_Binaire_Objet/Calc_Binaire_Objet/NombreBinaire.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calc_Binaire_Objet
+{
+    internal class NombreBinaire
+    {
+		private ushort[] _nombre;
+
+		public ushort[] Nombre
+		{
+			get { return _nombre; }
+			set { _nombre = value; }
+		}
+
+		public NombreBinaire(string nombre)
+		{
+			_nombre = RemplirTableau(nombre);
+		}
+
+		public NombreBinaire()
+		{
+			_nombre = new ushort[8];
+		}
+
+		public ushort[] RemplirTableau(string _nombre)
+		{
+			ushort[] tabBin = new ushort[8];
+			for(int i =0; i <= 7; i++)
+			{
+				tabBin[i] = 0;
+			}
+			for(int i =0; i <= _nombre.Length - 1; i++)
+			{
+				tabBin[7 - i] = ushort.Parse(_nombre[_nombre.Length - 1 - i].ToString());
+			}
+			return tabBin;
+		}
+
+		public void Additionne(NombreBinaire t2, out NombreBinaire tRes, out bool ok)
+		{
+			ok = true;
+			ushort report = 0;
+			tRes = new NombreBinaire();
+			for(int i  = 7; i >= 0; i--)
+			{
+				ushort res = (ushort)( Nombre[i] + t2.Nombre[i] + report);
+				if((int)(res/2) == 0)
+				{
+					report = 0;
+				}
+				else
+				{
+					report = 1;
+				}
+
+				if(res == 1)
+				{
+					tRes.Nombre[i] = 1;
+				}
+				else
+				{
+					if (res % 2 == 1) { 
+						tRes.Nombre[i] = 1;
+					}
+					else
+					{
+						tRes.Nombre[i] = 0;
+					}
+				}
+			}
+			if(report == 1)
+			{
+				ok = false;
+			}
+		}
+
+		public bool Soustrait(NombreBinaire t2, out NombreBinaire tRes)
+		{
+			int[] tTemp = new int[8];
+			bool ok = true;
+            tRes = new NombreBinaire();
+
+            for (int i = 0; i < 8; i++)
+			{
+				tTemp[i] = Nombre[i] - t2.Nombre[i];
+			}
+			for(int i = 7; i >= 1; i--)
+			{
+				if(tTemp[i] == -1)
+				{
+					t2.Nombre[i - 1] =(ushort)(t2.Nombre[i - 1] + 1);
+					Nombre[i] = (ushort)(Nombre[i] + 2);
+				}
+
+				if (Nombre[i] < t2.Nombre[i])
+				{
+					t2.Nombre[i - 1] = (ushort)(t2.Nombre[i - 1] + 1);
+					Nombre[i] = (ushort)(Nombre[i] + 2);
+				}
+                tRes.Nombre[i] = (ushort)(Nombre[i] - t2.Nombre[i]);
+            }
+			if (Nombre[0] >= t2.Nombre[0])
+			{
+				tRes.Nombre[0] = (ushort)(Nombre[0] - t2.Nombre[0]);
+			}
+			else
+			{
+				ok = false;
+			}
+			return ok;
+		}
+
+		public string AffichageResultat()
+		{
+			string result = "";
+			for(int i = 0; i < Nombre.Length; i++)
+			{
+				result = result + Nombre[i];
+			}
+			return result;
+		}
+	}
+}
